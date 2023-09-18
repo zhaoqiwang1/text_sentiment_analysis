7446d21669c8d8eec694b2afd1f4821d091830d0
zhaoqiwang1 <wzqbewater@gmail.com>
2023-09-18 16:58:39 +0800

    xueqiu try

--- spider.py
+++ spider.py
@@ -8,35 +8,34 @@ from bs4 import BeautifulSoup
 headers = {
     "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.1.2 Safari/605.1.15"
 }
-names=[]
-# The information we need are likely stored in a html under many pages. The first for loop is to allow us read all pages of the targeting html.
-for start_num in range(0, 250, 25):
 # I actually not sure what exactly does the "f" do in the following line.
 # The video says this "f" helps us 格式化. I don't quite understand.
-    response = requests.get(f"https://movie.douban.com/top250?start={start_num}", headers=headers)
+response = requests.get("https://xueqiu.com/S/SH688981", headers=headers)
 # The following if statement helps us recognize if our request is succesful.
-    # if response.status_code >= 200 and response.status_code < 400:
-    #     print("请求成功")
-    # elif response.status_code >= 400 and response.status_code < 500:
-    #     print("请求失败，客户端错误")
-    # else:
-    #     print("请求失败，服务器错误")
-    #     print(response)
-    #     print(response.status_code)
+if response.status_code >= 200 and response.status_code < 400:
+    print("请求成功")
+elif response.status_code >= 400 and response.status_code < 500:
+    print("请求失败，客户端错误")
+else:
+    print("请求失败，服务器错误")
+    print(response)
+    print(response.status_code)
 # We store all text information of this html as "html"
-    html = response.text
-    soup = BeautifulSoup(html, "html.parser")
+html = response.text
+print(html)
+soup = BeautifulSoup(html, "html.parser")
 # We need to go back to the targeting website to find the locations of the information we want to find.
-    all_titles = soup.findAll("span", attrs={"class": "title"})
-    for title in all_titles:
-        title_string = title.string
-        if "/" not in title_string:
-            names.append(title_string)
-print(names)
-print(len(names))
+all_contents = soup.findAll("span", attrs={"class": "source"})
+# for content in all_contents:
+#     print(content)
+#     content_string = content.string
+#     if "/" not in content_string:
+#        names.append(content_string)
+# print(names)
+# print(len(names))
 
-# What's next? Learn how to store the above printed targeting information into an excel file!
-data = {'Movie Names':names}
-df=pd.DataFrame(data)
-df.to_excel("/Users/zwang/Documents/Research/motivation_text_sentiment/text_sentiment_analysis/output.xlsx", sheet_name='Sheet1', startrow=0, startcol=0)
+## Store the above printed targeting information into an excel file!
+# data = {'Movie Names':names}
+# df=pd.DataFrame(data)
+# df.to_excel("/Users/zwang/Documents/Research/motivation_text_sentiment/text_sentiment_analysis/output.xlsx", sheet_name='Sheet1', startrow=0, startcol=0)
 
