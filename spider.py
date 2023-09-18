32abaa1062f1a284976b0d2fe02019d858a88b4f
zhaoqiwang1 <wzqbewater@gmail.com>
2023-09-18 18:39:20 +0800

    try

--- spider.py
+++ spider.py
@@ -3,6 +3,7 @@
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
 # The following "headers" command helps us pretend to be a normal browser, instead of a crawler, so that
 # the targeting html won't block our requests.
 headers = {
@@ -10,7 +11,7 @@ headers = {
 }
 # I actually not sure what exactly does the "f" do in the following line.
 # The video says this "f" helps us 格式化. I don't quite understand.
-response = requests.get("https://xueqiu.com/S/SH688981", headers=headers)
+response = requests.get("http://guba.eastmoney.com/list,300897,99.html", headers=headers)
 # The following if statement helps us recognize if our request is succesful.
 if response.status_code >= 200 and response.status_code < 400:
     print("请求成功")
@@ -22,20 +23,26 @@ else:
     print(response.status_code)
 # We store all text information of this html as "html"
 html = response.text
-print(html)
+#print(html)
 soup = BeautifulSoup(html, "html.parser")
 # We need to go back to the targeting website to find the locations of the information we want to find.
-all_contents = soup.findAll("span", attrs={"class": "source"})
-# for content in all_contents:
-#     print(content)
-#     content_string = content.string
-#     if "/" not in content_string:
-#        names.append(content_string)
-# print(names)
-# print(len(names))
+comments=[]
+all_comments = soup.findAll("div", attrs={"class": "title"})
+for content in all_comments:
+ #   print(content)
+    content_string = content.string
+    comments.append(content_string)
+# print(comments)
+# print(len(comments))
+
+authors = []
+all_authors = soup.findAll("div", attrs={"class": "author cl"})
+for author in all_authors:
+    print(author)
+
 
 ## Store the above printed targeting information into an excel file!
-# data = {'Movie Names':names}
+# data = {'Comments':comments}
 # df=pd.DataFrame(data)
 # df.to_excel("/Users/zwang/Documents/Research/motivation_text_sentiment/text_sentiment_analysis/output.xlsx", sheet_name='Sheet1', startrow=0, startcol=0)
 
